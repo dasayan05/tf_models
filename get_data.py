d005a8cec8b6aa1b66b0f44576a893305584cfeb
Ayan Das <dasayan05@hotmail.com>
2017-11-13 17:57:21 +0530

    mnist mini dataset of 500 for conven.

--- get_data.py
+++ get_data.py
@@ -13,3 +13,16 @@ def get_iris_data(return_X_y=True, one_hot=True):
         return X, L, {'n_sample': N, 'dim': d, 'n_class': C}
     else:
         return X, Y, {'n_sample': N, 'dim': d, 'n_class': C}
+
+def get_mini_mnist(file='mnist_mini.data', bsize=None, as_image=False):
+    import pickle
+
+    with open(file, 'rb') as f:
+        D, L = pickle.load(f)
+        if as_image:
+            D = D.reshape((-1, 28, 28))
+
+    if bsize == None:
+        return D, L
+    else:
+        return D[:bsize,...], L[:bsize,...]
